178600394673de76235f5635c1ce25d5283395b1
robin <wobula@gmail.com>
2017-09-29 11:43:43 -0700

    Sprucing up input to include individual complete keypresses.

--- classes/input.js
+++ classes/input.js
@@ -1,19 +1,36 @@
 class input {
 	constructor(avatar) {
+		this.player = avatar;
 		this.cursors = game.input.keyboard.createCursorKeys();
 		this.space = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+		this.m = game.input.keyboard.addKey(Phaser.Keyboard.M);
+		this.m.press = false;
 		game.input.keyboard.addKeyCapture([Phaser.Keyboard.SPACEBAR]);
-
-		this.player = avatar;
+		game.input.keyboard.addKeyCapture([Phaser.Keyboard.M]);
+		this.keys = [];
+		this.keys.push(this.m, this.space);
+		console.log(this.keys);
 	}
 	processor() {
 		if (this.cursors.up.isDown)
-			this.player.character.position.y -= 1;
+			this.player.character.body.velocity.y -= 1;
 		if (this.cursors.down.isDown)
-			this.player.character.position.y += 1;
+			this.player.character.body.velocity.y += 1;
 		if (this.cursors.left.isDown)
-			this.player.character.position.x -= 1;
-		if (this.cursors.right.isDown)
-			this.player.character.position.x += 1;
+			this.player.character.body.velocity.x -= 5;
+		if (this.cursors.right.isDown) {
+			this.player.character.body.velocity.x += 5;
+		}
+		for (var i = this.keys.length - 1; i >= 0; i--)
+			this.keyPress(this.keys[i]);
+	}
+	keyPress(key) {
+		if (key.isDown)
+			key.press = true;
+		if (key.press == true && key.isUp) {
+			console.log('you just pressed ' + key.event.code + ' key!');
+			this.player.character.body.velocity.y -= 100;
+			key.press = false;
+		}
 	}
 }
