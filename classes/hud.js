379b3d3b8167cb2801ea38cc87f90b7a768f355b
Robin Schramm <wobula@gmail.com>
2017-09-30 11:04:10 -0700

    Adding inventory system.

--- classes/hud.js
+++ classes/hud.js
@@ -1,16 +1,37 @@
 class HUD {
 	constructor() {
-		this.menu = game.add.button(15, 345, 'menu', this.actionOnClick, this, 2, 1, 0);
-		this.menu.onInputOver.add(this.over, this);
-		this.menu.onInputOut.add(this.out, this);
-		this.menu.scale.setTo(.15, .15);
-		this.menu.alpha = .5;
+		this.menuBtn = game.add.button(15, 345, 'menu', this.actionOnClick, this, 2, 1, 0);
+		this.menuBtn.onInputOver.add(this.over, this);
+		this.menuBtn.onInputOut.add(this.out, this);
+		this.menuBtn.scale.setTo(.15, .15);
+		this.menuBtn.alpha = .5;
 
-		this.inventory = game.add.button(45, 345, 'inventory', this.actionOnClick, this, 2, 1, 0);
-		this.inventory.onInputOver.add(this.over, this);
-		this.inventory.onInputOut.add(this.out, this);
-		this.inventory.scale.setTo(.15, .15);
-		this.inventory.alpha = .5;
+		this.invenBtn = game.add.button(45, 345, 'inventory', this.invenBtnAction, this, 2, 1, 0);
+		this.invenBtn.onInputOver.add(this.over, this);
+		this.invenBtn.onInputOut.add(this.out, this);
+		this.invenBtn.scale.setTo(.15, .15);
+		this.invenBtn.alpha = .5;
+		this.invenBtn.action = () => { this.invenBtnAction(); };
+
+		this.hat;
+		this.shoes;
+		this.shirt;
+		this.pants;
+		this.inventory = this.makeInventory();
+	}
+	makeInventory() {
+		this.hat = game.add.button(45, 315, 'menu', this.actionOnClick, this, 2, 1, 0);
+		//this.hat.onInputOver.add(this.over, this);
+		//this.hat.onInputOut.add(this.out, this);
+		this.hat.scale.setTo(.15, .15);
+		this.hat.alpha = 0;
+	}
+	invenBtnAction() {
+		console.log('You just clicked: ' + arguments[0].key);
+		if (this.hat.alpha == 0)
+			this.hat.alpha = 1;
+		else
+			this.hat.alpha = 0;
 	}
 	over() {
 		console.log('button over: ' + arguments[0].key);
