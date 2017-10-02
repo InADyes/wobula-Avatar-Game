db43bfe04c65b6789cb32ca1cc7148fc708df627
robin <wobula@gmail.com>
2017-10-02 11:56:13 -0700

    Individualizing character body parts.

--- classes/playerTest.js
+++ classes/playerTest.js
@@ -9,18 +9,35 @@ class playerTest {
 		this.character.body.bounce.y = 0.8;
 		this.character.anchor.setTo(.4, .3);
 
+		this.addNewCharacter();
+
+		this.bloodGeyser = game.add.emitter(0, 0, 999);
+		this.bloodGeyser.bounce.setTo(.5, .5);
+		this.bloodGeyser.setXSpeed(800, -200);
+		this.bloodGeyser.setYSpeed(500, 0);
+		this.bloodGeyser.minParticleScale = .70;
+		this.bloodGeyser.maxParticleScale = .50;
+		this.bloodGeyser.makeParticles('bloodDrop1', 0, 250, true, true);
+		this.character.addChild(this.bloodGeyser);
+		this.bloodGeyser.start(false, 5000, 1);
+		console.log(this.bloodGeyser.maxParticles);
+	}
+	addNewCharacter() {
 		this.characterHead = game.add.sprite(50, 220, 'stickHead');
 		this.characterHead.scale.setTo(.25, .25);
+
+		this.characterHead.ptr = {x: this.characterHead.position.x, y: this.characterHead.position.y};
+		console.log(this.characterHead.ptr);
 		/*game.physics.enable(this.characterHead, Phaser.Physics.ARCADE);
 		this.characterHead.body.collideWorldBounds = true;
 		this.characterHead.body.gravity.y = 500;
 		this.characterHead.body.bounce.y = 0.8;
 		//this.characterHead.anchor.setTo(.4, .3);*/
-
-		this.characterChest = game.add.sprite(48, 256, 'stickTorso');
+		console.log(this.characterHead.ptr);
+		this.characterChest = game.add.sprite(this.characterHead.ptr.x, this.characterHead.ptr.y + 20, 'stickChest');
 		this.characterChest.scale.setTo(.25, .25);
 
-		this.characterChest = game.add.sprite(50, 232, 'stickChest');
+		this.characterChest = game.add.sprite(50, 250, 'stickTorso');
 		this.characterChest.scale.setTo(.25, .25);
 
 		this.characterArm1 = game.add.sprite(41, 232, 'stickArm1');
@@ -34,16 +51,5 @@ class playerTest {
 
 		this.characterLeg2 = game.add.sprite(56, 266, 'stickLeg2');
 		this.characterLeg2.scale.setTo(.25, .25);
-
-		this.bloodGeyser = game.add.emitter(0, 0, 999);
-		this.bloodGeyser.bounce.setTo(.5, .5);
-		this.bloodGeyser.setXSpeed(800, -200);
-		this.bloodGeyser.setYSpeed(500, 0);
-		this.bloodGeyser.minParticleScale = .70;
-		this.bloodGeyser.maxParticleScale = .50;
-		this.bloodGeyser.makeParticles('bloodDrop1', 0, 250, true, true);
-		this.character.addChild(this.bloodGeyser);
-		this.bloodGeyser.start(false, 5000, 1);
-		console.log(this.bloodGeyser.maxParticles);
 	}
 }
