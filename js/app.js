318627d5f11fd09e8846bbfb4ce5170f38021a6f
robin <wobula@gmail.com>
2017-10-06 11:00:44 -0700

    Adding event listeners.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,13 @@
 var game = new Phaser.Game(600, 400, Phaser.CANVAS, 'canvas', { preload: preload, create: create, update: update });
 var data;
 
+var test;
+
+
 function preload() {
 	data = new mediaLibrary();
+	test = getDataHTML();
+	console.log(test.char);
 }
 
 var avatar;
@@ -19,4 +24,17 @@ function create() {
 function update() {
 	controls.processor();
 	game.physics.arcade.collide(env.floor, avatar.character);
+}
+
+function getDataHTML() {
+	var blah = {};
+	var blah2;
+
+	blah.char = document.getElementById("full").value;
+	blah.event = document.getElementById("full");
+	console.log(blah.event);
+	blah.event.addEventListener('change', function() {
+		alert(this.value);
+	}, false);
+	return (blah);
 }
