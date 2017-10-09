11d0e73b8aacee9c365e343f050c191b88cd0564
robin <wobula@gmail.com>
2017-10-09 10:56:53 -0700

    Input fix

--- classes/input.js
+++ classes/input.js
@@ -67,15 +67,11 @@ class input {
 		if (this.cursors.down.isDown)
 			this.player.characterBox.body.velocity.y += 1;
 		if (this.cursors.left.isDown) {
-
 			this.player.characterBox.body.velocity.x -= 5;
-			this.player.characterLegLBack2.angle -= 1;
 			//this.player.animations.walk();
 		}
 		if (this.cursors.right.isDown) {
 			this.player.characterBox.body.velocity.x += 5;
-			this.player.characterLegLBack2.angle += 1;
-
 			//this.player.animations.walk();
 		}
 		if (this.cursors.left.isUp && this.player.characterBox.body.velocity.x < 0)
@@ -130,7 +126,8 @@ class input {
 	hAction() {
 		console.log('h key');
 		this.player.characterLegLBack1.angle += 1;
-	vAction(){
+	}
+	vAction() {
 		console.log('You pressed the v key');
 		//avatar.leftarmGeyser.flow(1000, 500, 15, -1);
 		//avatar.rightarmGeyser.flow(1000, 500, 15, -1);
