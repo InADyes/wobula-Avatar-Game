d777280d9420ce97b024c40378017d95771a849f
InADyes <liuhao0320@gmail.com>
2017-10-13 15:48:39 -0700

    try to add tiger stuff

---
+++ classes/animationTiger.js
@@ -0,0 +1,165 @@
+class animationTiger {
+	constructor(avatar) {
+		this.now = game.time.time;
+		this.targetTime = undefined;
+		this.avatar = avatar;
+		this.walkSetup(this.avatar);
+		this.waveRightSetup(this.avatar);
+		this.waveLeftSetup(this.avatar);
+		this.breathSetup(this.avatar);
+		this.walkBouncesetup(this.avatar);
+	}
+
+	processor() {
+		if (this.avatar.characterBox.body.velocity.x != 0)
+			{
+			this.walk();
+			this.walkBounce();
+			}
+	}
+
+	walk(direction) {
+		this.walkArms(direction, this.avatar);
+		this.walkLegs(direction, this.avatar);
+	}
+	waveLeft() {
+		this.arm1Wave.start();
+	}
+	waveRight() {
+		this.arm2Wave.start();
+	}
+	walkArms(direction) {
+		if (this.avatar.characterArm1.angle == -10) {
+			this.arm1.start();
+			this.arm2Rev.start();
+		} else if (avatar.characterArm1.angle == -110) {
+			this.arm2.start();
+			this.arm1Rev.start();
+		}
+	}
+	walkLegs() {
+		if (avatar.characterLeg1.angle == -20) {
+			this.leg1.start();
+			this.leg2Rev.start();
+		} else if (avatar.characterLeg1.angle == 20) {
+			this.leg1Rev.start();
+			this.leg2.start();
+		}
+	}
+	breathe() {
+		if (avatar.characterChest.scale.x == 1.2)
+			this.breathOut.start();
+		else if (avatar.characterChest.scale.x == 1)
+			this.breathIn.start();
+	}
+	breathSetup(avatar) {
+		this.breathIn = game.add.tween(avatar.characterChest.scale);
+		this.breathIn.to({ x: 1.2, y: 1.2 }, 1000, Phaser.Easing.Linear.None);
+		this.breathOut = game.add.tween(avatar.characterChest.scale);
+		this.breathOut.to({ x: 1, y: 1 }, 4000, Phaser.Easing.Linear.None);
+	}
+	walkSetup(avatar) {
+		avatar.characterLeg1.angle = -20;
+		this.leg1 = game.add.tween(avatar.characterLeg1);
+		this.leg1.to({ angle: 20 }, 550, Phaser.Easing.Quadratic.InOut);
+		this.leg2 = game.add.tween(avatar.characterLeg2);
+		this.leg2.to({ angle: 20 }, 550, Phaser.Easing.Quadratic.InOut);
+
+		this.leg1Rev = game.add.tween(avatar.characterLeg1);
+		this.leg1Rev.to({ angle: -20 }, 550, Phaser.Easing.Quadratic.InOut);
+		this.leg2Rev = game.add.tween(avatar.characterLeg2);
+		this.leg2Rev.to({ angle: -20 }, 550, Phaser.Easing.Quadratic.InOut);
+
+		avatar.characterArm1.angle = -10;
+		this.arm1 = game.add.tween(avatar.characterArm1);
+		this.arm1.to({ angle: -110 }, 550, Phaser.Easing.Quadratic.InOut);
+		this.arm1Rev = game.add.tween(avatar.characterArm1);
+		this.arm1Rev.to({ angle: -10 }, 550, Phaser.Easing.Quadratic.InOut);
+
+		this.arm2 = game.add.tween(avatar.characterArm2);
+		this.arm2.to({ angle: -10 }, 550, Phaser.Easing.Quadratic.InOut);
+		this.arm2Rev = game.add.tween(avatar.characterArm2);
+		this.arm2Rev.to({ angle: 90 }, 550, Phaser.Easing.Quadratic.InOut);
+	}
+	waveLeftSetup(avatar) {
+		this.arm1Wave = game.add.tween(avatar.characterArm1);
+		this.arm1Wave.to({ angle: 180 }, 600, Phaser.Easing.Linear.None);
+		this.waveSidetoSide(5, 'left');
+		this.arm1Wave.to({ angle: -10 }, 600, Phaser.Easing.Linear.None);
+	}
+	waveRightSetup(avatar) {
+		this.arm2Wave = game.add.tween(avatar.characterArm2);
+		this.arm2Wave.to({ angle: -180 }, 600, Phaser.Easing.Linear.None);
+		this.waveSidetoSide(5, 'right');
+		this.arm2Wave.to({ angle: -10 }, 600, Phaser.Easing.Linear.None);
+	}
+	waveSidetoSide(repeat, side) {
+		if (side == 'left') {
+			while (repeat > 0) {
+				this.arm1Wave.to({ angle: 80 }, 100, Phaser.Easing.Linear.None);
+				this.arm1Wave.to({ angle: 140 }, 100, Phaser.Easing.Linear.None);
+				repeat--;
+			}
+		} else if (side == 'right') {
+			while (repeat > 0) {
+				this.arm2Wave.to({ angle: -80 }, 100, Phaser.Easing.Linear.None);
+				this.arm2Wave.to({ angle: -140 }, 100, Phaser.Easing.Linear.None);
+				repeat--;
+			}
+		}
+	}
+	walkSideToSide(targetTime, direction) {
+		this.now = game.time.time;
+		if (this.targetTime == undefined) {
+			console.log(targetTime);
+			this.targetTime = game.time.time + targetTime;
+		}
+		if (this.now < this.targetTime) {
+			if (direction == 'right')
+				controls.w.isDown = true;
+			else
+				controls.q.isDown = true;
+			return (1);
+		}
+		if (controls.w.isDown == true)
+			controls.w.isDown = false;
+		else if (controls.q.isDown = true)
+			controls.q.isDown = false;
+		this.targetTime = undefined;
+		return (0);
+	}
+
+	walkBouncesetup(avatar) {
+
+		this.headbounceup = game.add.tween(avatar.characterHead);
+		this.chestbounceup = game.add.tween(avatar.characterChest);
+		this.headbounceup.to({ y: -33 }, 400, Phaser.Easing.Linear.None);
+		this.chestbounceup.to({ y: -3 }, 400, Phaser.Easing.Linear.None);
+
+		this.headbouncedown = game.add.tween(avatar.characterHead);
+		this.chestbouncedown = game.add.tween(avatar.characterChest);
+		this.headbouncedown.to({ y: -30 }, 400, Phaser.Easing.Linear.None);
+		this.chestbouncedown.to({ y: 0 }, 400, Phaser.Easing.Linear.None);
+	}
+
+	walkBounce() {
+		if (avatar.characterHead.y == -30) {
+			this.headbounceup.start();
+			this.chestbounceup.start();
+		}
+		if (avatar.characterHead.y == -33) {
+			this.headbouncedown.start();
+			this.chestbouncedown.start();
+		}
+	}
+
+	randomWalk() {
+		if ((avatar.animations.walkSideToSide(random(5000, 200), direction)) == 0) {
+			if (random(6, 1) > 3)
+				direction = 'right';
+			else
+				direction = 'left';
+			console.log(direction);
+		}
+	}
+}
---
+++ classes/tiger.js
@@ -0,0 +1,97 @@
+class tiger {
+	constructor(name) {
+		this.name = name;
+		this.addBodyParts();
+		this.bindBody();
+		this.physics();
+		this.addGeysers();
+		//this.animations = new animation(this, 'player');
+	}
+	addBodyParts() {
+		this.characterBox = game.add.sprite(50, 100);
+		this.headblood = game.add.sprite(0, -195);
+		//this.headblood.scale.setTo(.3, .3);
+		this.arm1blood = game.add.sprite(-20, -165);
+		//this.arm1blood.scale.setTo(.3, .3);
+		this.arm2blood = game.add.sprite(25, -160);
+		//this.arm2blood.scale.setTo(.3, .3);
+		this.torsoblood = game.add.sprite(0, -165);
+		//this.torsoblood.scale.setTo(.3, .3);
+		this.leg1blood = game.add.sprite(-15, -120);
+		//this.leg1blood.scale.setTo(.3, .3);
+		this.leg2blood = game.add.sprite(25, -120);
+		//this.leg2blood.scale.setTo(.3, .3);
+		this.pelvisblood = game.add.sprite(5, -130);
+		//this.pelvisblood.scale.setTo(.3, .3);
+
+		this.characterHead = game.add.sprite(0, -30, 'duckhead');
+		//this.characterHead.scale.setTo(.3, .3);
+		this.characterHead.anchor.setTo(.5, .5);
+		this.characterArm2 = game.add.sprite(8, -10, 'duckarm2');
+		//this.characterArm2.scale.setTo(.3, .3);
+		this.characterArm2.anchor.setTo(0, 0);
+		this.characterChest = game.add.sprite(0, 0, 'duckbody');
+		//this.characterChest.scale.setTo(.3, .3);
+		this.characterChest.anchor.setTo(.5, .5);
+		this.characterArm1 = game.add.sprite(-8, -10, 'duckarm1');
+		//this.characterArm1.scale.setTo(.3, .3);
+		this.characterArm1.anchor.setTo(1, 0);
+		this.characterLeg1 = game.add.sprite(-8, 15, 'duckleg1');
+		//this.characterLeg1.scale.setTo(.3, .3);
+		this.characterLeg1.anchor.setTo(0, 0);
+		this.characterLeg2 = game.add.sprite(-18, 15, 'duckleg2');
+		//this.characterLeg2.scale.setTo(.3, .3);
+		this.characterLeg2.anchor.setTo(0, 0);
+		this.characterPelvis = game.add.sprite(5, 10, 'char1Pelvis');
+		this.characterPelvis.scale.setTo(.01, .01);
+		this.characterPelvis.anchor.setTo(.5, .5);
+	}
+	addGeysers() {
+		this.headGeyser = this.addIndividualGeysers(this.characterHead, 0, 0);
+		//this.headGeyser = this.addIndividualGeysers(this.characterHead, this.characterHead.width/2, this.characterHead.height/2);
+		this.chestGeyser = this.addIndividualGeysers(this.characterChest, 0, 0);
+		this.pelvisGeyser = this.addIndividualGeysers(this.characterPelvis, 0, 0);
+		this.leftarmGeyser = this.addIndividualGeysers(this.arm1blood, 0, 0);
+		this.rightarmGeyser = this.addIndividualGeysers(this.arm2blood, 0, 0);
+		this.leftlegGeyser = this.addIndividualGeysers(this.leg1blood, 0, 0);
+		this.rightlegGeyser = this.addIndividualGeysers(this.leg2blood, 0, 0);
+		//this.leftarmGeyser = this.addIndividualGeysers(this.characterArm1,0,0);
+	}
+
+	addIndividualGeysers(anchor, xoffset, yoffset) {
+		var bloodGeyser = game.add.emitter(0 + xoffset, 0 + yoffset, 999);
+		bloodGeyser.bounce.setTo(.5, .5);
+		bloodGeyser.setXSpeed(800, -200);
+		bloodGeyser.setYSpeed(500, 0);
+		bloodGeyser.minParticleScale = .70;
+		bloodGeyser.maxParticleScale = .50;
+		bloodGeyser.makeParticles(particlesource, 0, 250, true, true);
+		anchor.addChild(bloodGeyser);
+		bloodGeyser.start(false, 5000, 1);
+		return(bloodGeyser);
+	}
+
+	bindBody() {
+		this.characterBox.addChild(this.characterHead);
+		this.characterBox.addChild(this.characterArm1);
+		this.characterBox.addChild(this.characterChest);
+		this.characterBox.addChild(this.characterPelvis);
+		this.characterBox.addChild(this.characterLeg1);
+		this.characterBox.addChild(this.characterLeg2);
+		this.characterBox.addChild(this.characterArm2);
+		this.characterBox.addChild(this.headblood);
+		this.characterBox.addChild(this.arm1blood);
+		this.characterBox.addChild(this.torsoblood);
+		this.characterBox.addChild(this.pelvisblood);
+		this.characterBox.addChild(this.arm2blood);
+		this.characterBox.addChild(this.leg1blood);
+		this.characterBox.addChild(this.leg2blood);
+		//this.characterBox.scale.setTo(.3, .3);
+	}
+	physics() {
+		game.physics.enable(this.characterBox, Phaser.Physics.ARCADE);
+		this.characterBox.body.collideWorldBounds = true;
+		this.characterBox.body.gravity.y = 500;
+		this.characterBox.body.bounce.y = 0.6;
+	}
+}
